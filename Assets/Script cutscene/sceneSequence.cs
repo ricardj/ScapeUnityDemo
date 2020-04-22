0d27092054a2f5b585c8eca9c8641e7d549128b2
ricardj <43671359+ricardj@users.noreply.github.com>
2020-04-22 17:18:32 +0200

    Unity prooject added

---
+++ Assets/Script cutscene/sceneSequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneSequence : MonoBehaviour
+{
+
+    public GameObject cam1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TheSequence()); 
+    }
+
+    IEnumerator TheSequence()
+    {
+        yield return new WaitForSeconds(4);
+        cam1.SetActive(true);
+        yield return new WaitForSeconds(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
